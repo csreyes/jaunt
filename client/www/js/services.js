24eab671da645a6a14eb9d6799fcfc866672b868
mc <mc@mcs-MacBook-Pro.local>
2015-02-05 21:38:00 -0800

    Updates controllers for loading pages
    Navigate controller loading changes
    
    Creates startJaunt functionality within map view
    
    Merge conflicts resolved

--- client/www/js/services.js
+++ client/www/js/services.js
@@ -90,3 +90,21 @@ angular.module('starter.services')
     }
   }
 });
+
+
+// $scope.createMap = function(position, zoom) {
+//   console.log('create map called');
+//   var mapOptions = {
+//     center: position,
+//     zoom: zoom,
+//     mapTypeId: google.maps.MapTypeId.ROADMAP,
+//     draggableCursor:'grab',
+//     mapTypeControl: false,
+//     panControl: false,
+//     zoomControl: false,
+//     streetViewControl: false,
+//     styles: [{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"color":"#f7f1df"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"color":"#d0e3b4"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.medical","elementType":"geometry","stylers":[{"color":"#fbd3da"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#bde6ab"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffe15f"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#efd151"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"black"}]},{"featureType":"transit.station.airport","elementType":"geometry.fill","stylers":[{"color":"#cfb2db"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#a2daf2"}]}]
+//   };
+
+//   $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+// };
